47457530bcb95fc4bcee1dec80698149d9398c0b
cristianstoe <cristian3300@gmail.com>
2021-05-10 11:13:47 -0300

    Authentication and bug fixing of errors

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, request, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token Missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "26ce73cb3843e07f9545dc4d412978fd"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User dont exists", 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
+}
